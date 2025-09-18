6b4adc3c50330f0a28bf3927912bd648cfbff57c
Christopher El-Murr <christopher.el-murr@stu.ocsb.ca>
2025-09-18 01:22:19 +0000

    added basicMath

---
+++ basicMath.cpp
@@ -0,0 +1,15 @@
+// Created By: Christopher El-Murr
+// Date: 09 17, 2025
+// This program is a basic math calculator capable of solving basic math operations.
+
+#include <iostream>
+#include<cmath>
+
+int main() {
+    std::cout << "8+7=" << (8 + 7) << std::endl;
+    std::cout << "8-7=" << (8 - 7) << std::endl;
+    std::cout << "8/2=" << (8/2) << std::endl;
+    std::cout << "8*7=" << (8*7) << std::endl;
+    std::cout << "8+7/7=" << (8+7/7) << std::endl;
+    std::cout << "8+pow(7,4)=" << (8+pow(7,4)) << std::endl;
+}
